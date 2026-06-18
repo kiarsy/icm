e43091260c6a812d4c1473abf3587028acb58c6c
kiarsy <rkiarsy@ecotrak.com>
2026-06-18 14:07:12 +0300

    Init project structure

---
+++ ICMarkets.Api/Program.cs
@@ -0,0 +1,23 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "ICMarkets"));
+}
+
+app.UseHttpsRedirection();
+app.UseExceptionHandler();
+app.UseSerilogRequestLogging();
+app.MapControllers();
+app.Run();
